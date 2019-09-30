c7679b9f763d9df2d5899b791879439a292bf31d
Joy Best <fasterthanmomentum@gmail.com>
2019-09-30 19:52:59 -0400

    dragged navigation and tableview controller to the scene moved the entry point to the navigation controller deleted the initial view controller deleted the view controller swift file created ToDo swift file which the process of defining the model. created the ToDoTableViewController file--created an empty array of model objects in the ToDoTableViewController file

---
+++ ToDoList9302019/ToDo.swift
@@ -0,0 +1,19 @@
+//
+//  ToDo.swift
+//  ToDoList9302019
+//
+//  Created by JOY BEST on 9/30/19.
+//  Copyright © 2019 JOY BEST. All rights reserved.
+//
+
+//import Foundation
+//importing UIKit since import Foundation does not contain any platform-specific classes and UIImage is specific to IOS
+import UIKit
+//defining the model
+struct ToDo {
+    var title: String
+    var isComplete: Bool
+    var dueDate: Bool
+    var notes: String?
+
+}
---
+++ ToDoList9302019/ToDoTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList9302019
+//
+//  Created by JOY BEST on 9/30/19.
+//  Copyright © 2019 JOY BEST. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    //creating an empty array of model objects
+    var todos = [ToDo] ()
+}
